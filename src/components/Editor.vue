2e42846c0c70b54231dc216e1e6ce3320e06b465
Tanveer Iqbal <tan.acute.com>
2020-02-11 16:14:14 +0300

    Backend API connectvity

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,21 +1,21 @@
 <template>
-<div class="container">
+<div class="container is-fluid">
     <div class="notification">
-        This container is
-        <strong>centered</strong> on desktop.
+        <strong>XML to IData</strong> Transformation
+        <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="true"></b-loading>
     </div>
-    <div class="columns is-desktop">
+    <div class="columns">
         <div class="column">
             <div class="field">
                 <div class="control">
-                    <textarea v-model="inputTxt" class="textarea" placeholder="Large textarea" rows="10"></textarea>
+                    <textarea id="codemirrorInput" class="textarea" placeholder="Large textarea" rows="10"></textarea>
                 </div>
             </div>
         </div>
         <div class="column">
             <div class="field">
                 <div class="control">
-                    <textarea v-model="outputTxt" class="textarea" placeholder="Large textarea" rows="10"></textarea>
+                    <textarea id="codemirrorOutput" class="textarea" placeholder="Large textarea" rows="10"></textarea>
                 </div>
             </div>
         </div>
@@ -25,48 +25,111 @@
 </template>
 
 <script>
-//import axios from "axios"
+import axios from "axios"
+import 'codemirror/lib/codemirror.css'
+  // theme css
+import 'codemirror/theme/monokai.css'
+import CodeMirror from 'codemirror'
+    // language
+import 'codemirror/mode/xml/xml.js'
+
+// require active-line.js
+  import'codemirror/addon/selection/active-line.js'
+  // styleSelectedText
+  import'codemirror/addon/selection/mark-selection.js'
+  import'codemirror/addon/search/searchcursor.js'
+  // hint
+  import'codemirror/addon/hint/show-hint.js'
+  import'codemirror/addon/hint/show-hint.css'
+  import'codemirror/addon/hint/javascript-hint.js'
+  import'codemirror/addon/selection/active-line.js'
+  // highlightSelectionMatches
+  import'codemirror/addon/scroll/annotatescrollbar.js'
+  import'codemirror/addon/search/matchesonscrollbar.js'
+  import'codemirror/addon/search/searchcursor.js'
+  import'codemirror/addon/search/match-highlighter.js'
+  // keyMap
+  import'codemirror/mode/clike/clike.js'
+  import'codemirror/addon/edit/matchbrackets.js'
+  import'codemirror/addon/comment/comment.js'
+  import'codemirror/addon/dialog/dialog.js'
+  import'codemirror/addon/dialog/dialog.css'
+  import'codemirror/addon/search/searchcursor.js'
+  import'codemirror/addon/search/search.js'
+  import'codemirror/keymap/sublime.js'
+  // foldGutter
+  import'codemirror/addon/fold/foldgutter.css'
+  import'codemirror/addon/fold/brace-fold.js'
+  import'codemirror/addon/fold/comment-fold.js'
+  import'codemirror/addon/fold/foldcode.js'
+  import'codemirror/addon/fold/foldgutter.js'
+  import'codemirror/addon/fold/indent-fold.js'
+  import'codemirror/addon/fold/markdown-fold.js'
+  import'codemirror/addon/fold/xml-fold.js'
 export default {
   data() {
     return {
-      inputTxt: "",
-      outputTxt: "",
-      username: "Adminstrator",
-      password: "manage"
+      codeMirrorInput: "",
+      codeMirrorOutput: "",
+      isLoading: false,
+      isFullPage: true
     };
   },
   methods: {
        transform() {
-        //  let url = "https://piktl7s9ae.execute-api.us-east-1.amazonaws.com/prod/transform";
-        //let username = this.username;
-        //let password = this.password;
-        //const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64')
-    //     axios.post(url, {
-    //        headers: {
-    //            'Content-Type': 'application/xml',
-    //            'x-request-type': 'xml-idata',
-    //            //'Authorization': `Basic ${token}`
-    //        },
-    //        body: this.inputTxt
-    //     })
-    //    .then(function (response) {
-    //     console.log(response);
-    //     })
-    //    .catch(function (error) {
-    //     console.log(error);
-    //     });
-      this.outputTxt = `<?xml version="1.0" encoding="UTF-8"?>
-<IDataXMLCoder version="1.0">
-    <record javaclass="com.wm.util.Values">
-        <record name="note" javaclass="com.wm.data.BasicData">
-            <value name="to">Tanveer</value>
-            <value name="from">Jani</value>
-            <value name="heading">Reminder</value>
-            <value name="body">Don't forget me this weekend!</value>
-        </record>
-    </record>
-</IDataXMLCoder>`
+      let xmlRequest = this.codeMirrorInput.getValue();
+      this.isLoading = true;
+        let config = {
+            baseURL: 'https://webmapi-u4kuttf3wq-uc.a.run.app',
+            headers: { 'Content-Type': 'text/xml', 'x-request-type': 'xml-idata' },
+            timeout: 60000,
+            withCredentials: false,
+            auth: {
+             username: "Administrator",
+             password: "manage"
+            }
+        }
+        axios.post('/rest/TI_DevUtil/api/v2/transform', xmlRequest, config)
+         .then( response => {
+             this.codeMirrorOutput.setValue(response.data);
+             this.isLoading = false;
+         })
+         .catch( error => console.log(error))
+      
       }
+  },
+  mounted() {
+    const cmOption = {
+          tabSize: 4,
+          styleActiveLine: false,
+          lineNumbers: true,
+          styleSelectedText: false,
+          line: true,
+          foldGutter: true,
+          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+          highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true },
+          mode: 'text/xml',
+          // hint.js options
+          hintOptions:{
+            completeSingle: false
+          },
+          //sublim, emac, vim
+          keyMap: "sublime",
+          matchBrackets: true,
+          showCursorWhenSelecting: true,
+          theme: "default",
+          extraKeys: { "Ctrl": "autocomplete" }
+        }
+    this.codeMirrorInput = CodeMirror.fromTextArea(document.getElementById("codemirrorInput"), cmOption);
+    this.codeMirrorOutput = CodeMirror.fromTextArea(document.getElementById("codemirrorOutput"), cmOption);
+
   }
 };
 </script>
+
+<style scoped>
+.CodeMirror {
+  border: 1px solid #eee;
+  height: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Navbar, Input, Button } from 'buefy'
+import { Navbar, Input, Button, Loading } from 'buefy'
 import 'buefy/dist/buefy.css'
+
 
 Vue.config.productionTip = false
 Vue.use(Navbar)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Loading)
 
 new Vue({
   render: h => h(App),
